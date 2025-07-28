256e6971dbaba011ea95f2c0a2c60b1478dfce81
Abel liu <13478440986@163.com>
2025-07-29 05:57:10 +0800

    [.netcore_microservice][网关ocelot+Consul服务发现] 能够网关成功了， Abel

---
+++ Client/Program.cs
@@ -0,0 +1,16 @@
+﻿// Program.cs
+using DnsClient;
+using System.Net;
+
+var dnsEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8600); // Consul DNS 默认端口
+var client = new LookupClient(dnsEndpoint);
+
+// 查询服务
+var result = await client.QueryAsync("servicename.service.consul", QueryType.SRV);
+
+// 处理结果
+foreach (var record in result.Answers.SrvRecords())
+{
+    Console.WriteLine($"服务: {record.Target}:{record.Port}");
+    Console.WriteLine($"优先级: {record.Priority}, 权重: {record.Weight}");
+}
